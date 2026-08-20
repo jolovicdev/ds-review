aee5a6c3581cefeb82d87431e171e6ee8d38695d
jolovicdev <jolovic@pm.me>
2026-08-20 21:44:23 +0200

    Release 0.1.3

--- src/main.py
+++ src/main.py
@@ -42,7 +42,7 @@ async def lifespan(_app: FastAPI) -> AsyncIterator[None]:
     yield
 
 
-app = FastAPI(title="DS-Review", version="0.1.2", lifespan=lifespan)
+app = FastAPI(title="DS-Review", version="0.1.3", lifespan=lifespan)
 
 
 def verify_hmac(body: bytes, signature_header: str | None) -> bool:
